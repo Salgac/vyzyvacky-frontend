1e39df904175ac608dac451a41fa75d96884892c
Dominik <domco.salgovic@gmail.com>
2021-08-25 18:27:25 +0200

    Implemented table generation

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,9 +1,38 @@
 <template>
   <div class="table">
     <h1>{{ title }}</h1>
-    <ul>
-      <li v-for="item in people" :key="item">{{ item }}</li>
-    </ul>
+    <table class="table-hover" v-if="dataArr">
+      <thead>
+        <tr>
+          <th>#</th>
+          <!-- People specific fields -->
+          <th v-if="arrHasKey('firstName')">First Name</th>
+          <th v-if="arrHasKey('lastName')">Last Name</th>
+          <th v-if="arrHasKey('age')">Age</th>
+          <th v-if="arrHasKey('name')">Team</th>
+          <th v-if="arrHasKey('color')">Team Color</th>
+          <!-- Entries specific fields -->
+          <th v-if="arrHasKey('winner_id')">Winner</th>
+          <th v-if="arrHasKey('looser_id')">Looser</th>
+          <th v-if="arrHasKey('time')">Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in dataArr" :key="item">
+          <td>{{ index + 1 }}</td>
+          <!-- People specific fields -->
+          <td v-if="item.firstName">{{ item.firstName }}</td>
+          <td v-if="item.lastName">{{ item.lastName }}</td>
+          <td v-if="item.age">{{ item.age }}</td>
+          <td v-if="item.name">{{ item.name }}</td>
+          <td v-if="item.color">{{ item.color }}</td>
+          <!-- Entries specific fields -->
+          <td v-if="item.winner_id">{{ item.winner_id }}</td>
+          <td v-if="item.looser_id">{{ item.looser_id }}</td>
+          <td v-if="item.time">{{ item.time }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -19,7 +48,7 @@ export default defineComponent({
   },
   data() {
     return {
-      people: [],
+      dataArr: [{}],
     };
   },
   beforeMount() {
@@ -37,12 +66,16 @@ export default defineComponent({
       }).then(
         (result) => {
           console.log(result);
-          this.people = result.data;
+          this.dataArr = result.data;
         },
         (error) => {
           console.log(error);
         }
       );
+    },
+    arrHasKey(keyName: string) {
+      if (keyName in this.dataArr[0]) return true;
+      else return false;
     },
   },
 });
